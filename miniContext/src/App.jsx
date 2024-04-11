1446f93a49cef27aefc1653dc5a7bd9e8ac97377
Adesh <adeshrai707@gmail.com>
2024-04-11 20:53:29 +0530

    MiniContext

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import UserContextProvider from './context/UserContextProvider'
 import './App.css'
+import Login from './components/Login.jsx'
+import Profile from './components/Profile.jsx'
 
 function App() {
   return (
-    <>
+    <UserContextProvider>
      <h1>React with Chai</h1>
-    </>
+     <Login/>
+     <Profile/>
+    </UserContextProvider>
   )
 }
 
-export default App
+export default App  
